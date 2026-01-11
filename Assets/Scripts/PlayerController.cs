d7d9bed175b66bd3f6433c9de47b92a1065bb0ff
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2026-01-11 22:10:01 +0100

    Unity Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+[DefaultExecutionOrder(-1)]
+public class PlayerController : MonoBehaviour
+{
+    #region Class Variables
+    [Header("Components")]
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Camera _playerCamera;
+
+    [Header("Movement Settings")]
+    public float runAcceleration = 35f;
+    public float runSpeed = 4f;
+    public float sprintAcceleration = 50f;
+    public float sprintSpeed = 7f;
+    public float drag = 20f;
+    public float movingThreshold = 0.01f;
+
+    [Header("Camera Settings")]
+    public float lookSenseH = 0.1f;
+    public float lookSenseV = 0.1f;
+    public float lookLimitV = 89f;
+
+    private PlayerLocomotionInput _playerLocomotionInput;
+    private PlayerState _playerState;
+
+    private Vector2 _cameraRotation = Vector2.zero;
+    private Vector2 _playerTargetRotation = Vector2.zero;
+    #endregion
+
+    #region Startup Methods
+    private void Awake()
+    {
+        _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        _playerState = GetComponent<PlayerState>();
+    }
+    #endregion
+
+    #region Update Logic
+    private void Update()
+    {
+        UpdateMovementState();
+        HandleLateralMovement();
+    }
+
+    private void UpdateMovementState()
+    {
+        bool isMovementInput = _playerLocomotionInput.MovementInput != Vector2.zero;
+        bool isMovingLaterally = IsMovingLaterally();
+        bool isSprinting = _playerLocomotionInput.SprintToggledOn && isMovingLaterally;
+
+        PlayerMovementState lateralState = 
+            isSprinting ? PlayerMovementState.Sprinting :
+            isMovingLaterally || isMovementInput ? PlayerMovementState.Running : PlayerMovementState.Idling;
+
+        _playerState.SetPlayerMovementState(lateralState);
+    }
+
+    private void HandleLateralMovement()
+    {
+        bool isSprinting = _playerState.CurrentPlayerMovementState == PlayerMovementState.Sprinting;
+
+        float lateralAcceleration = isSprinting ? sprintAcceleration : runAcceleration;
+        float clampLateralMagnitude = isSprinting ? sprintSpeed : runSpeed;
+
+        Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0, _playerCamera.transform.forward.z).normalized;
+        Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0, _playerCamera.transform.right.z).normalized;
+        Vector3 movementDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y;
+
+        Vector3 movementDelta = movementDirection * lateralAcceleration;
+        Vector3 newVelocity = _characterController.velocity + movementDelta;
+
+        Vector3 currentDrag = newVelocity.normalized * drag;
+        newVelocity = (newVelocity.magnitude > drag) ? newVelocity - currentDrag : Vector3.zero;
+        newVelocity = Vector3.ClampMagnitude(newVelocity, clampLateralMagnitude);
+
+        _characterController.Move(newVelocity * Time.deltaTime);
+    }
+    #endregion
+
+    #region LateUpdate Logic
+    private void LateUpdate()
+    {
+        _cameraRotation.x += lookSenseH * _playerLocomotionInput.LookInput.x;
+        _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - lookSenseV * _playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
+
+        _playerTargetRotation.x += transform.eulerAngles.x + lookSenseH * _playerLocomotionInput.LookInput.x;
+        transform.rotation = Quaternion.Euler(0, _playerTargetRotation.x, 0);
+
+        _playerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0);
+    }
+    #endregion
+
+    #region State Checks
+    private bool IsMovingLaterally()
+    {
+        Vector3 lateralVelocity = new Vector3(_characterController.velocity.x, 0, _characterController.velocity.z);
+
+        return lateralVelocity.magnitude > movingThreshold;
+    }
+    #endregion
+}
